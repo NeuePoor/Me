358f66c52cbfc1aeae1611b9759a3c84d9937026
Fo <96456156+NeuePoor@users.noreply.github.com>
2026-04-04 02:53:53 +0800

    Update header.module.js

--- assets/js/header.module.js
+++ assets/js/header.module.js
@@ -1,1 +1,26 @@
-//header module
+export function initHeader(CONFIG) {
+  const gif = document.getElementById('ui-logo-gif');
+  const text = document.getElementById('ui-logo-text');
+  const nav = document.getElementById('ui-nav');
+  
+  text.innerText = CONFIG.header.logo.text;
+  gif.src = CONFIG.header.logo.gif;
+  
+  gif.onload = () => { 
+    gif.style.opacity = "1"; 
+    text.style.opacity = "0"; 
+  };
+  
+  gif.onerror = () => { 
+    gif.style.display = "none"; 
+    text.style.setProperty('opacity', '1', 'important'); 
+  };
+  
+  gif.onclick = () => { 
+    window.location.href = CONFIG.header.logo.link; 
+  };
+  
+  nav.innerHTML = CONFIG.header.nav.map(n => 
+    `<a href="${n.link}" class="fl-nav__link">${n.name}</a>`
+  ).join('');
+}
